ed9e79a08856d1b279d0b77e15000fe569c9481c
polmanovic <polman@MacBook-Pro-van-polman.local>
2016-03-07 15:53:11 +0100

    database connectie

--- server.js
+++ server.js
@@ -1,37 +1,25 @@
+var express    = require("express");
+var app = express();
 
-/*
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'example.org',
-  user     : 'bob',
-  password : 'secret',
-});
-
-connection.connect(function(err) {
-  // connected! (unless `err` is set)
-});
-
-*/
-
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-app.use(bodyParser.json());
-
-
-var mysql = require('mysql');
-function getConnection() {
-  var connection = mysql.createConnection({
-    host: '10.3.0.195',
-    user: 'frankjq108_frank',
+  	host: '10.3.0.195',
+    user: 'frankjq108',
     password: 'polman23',
-    database: 'frankjq108_todo'
-  });
-  return connection;
+    database: 'frankjq108_data'
+});
 
-  alert("connection");
-}
+connection.connect();
+
+connection.query('SELECT * from gebruikers', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+});
+
+connection.end();
 
 app.listen(3000, function () {
   console.log('inlog functie on port 3000');
