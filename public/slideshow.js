7d27dc6882f5d2d20c366ed0f4ce557ad26843ac
polmanovic <polman@MacBook-Pro-van-polman.local>
2016-03-08 14:25:42 +0100

    slideshow toegevoegd

---
+++ public/slideshow.js
@@ -0,0 +1,22 @@
+// run when doc ready
+// start de slideshow
+function slideSwitch( jQuery ) {
+    var $active = $('#slideshow IMG.active');
+
+    if ($active.length == 0) $active = $('#slideshow IMG:last');
+
+    var $next = $active.next().length ? $active.next()
+        : $('#slideshow IMG:first');
+
+    $active.addClass('last-active');
+
+    $next.css({ opacity: 0.0 })
+        .addClass('active')
+        .animate({ opacity: 1.0 }, 1000, function () {
+            $active.removeClass('active last-active');
+        });
+};
+
+$(function () {
+    setInterval("slideSwitch()", 5000);
+});
