0597d09a323f0394da28770ec60a4a94e0473fe0
polmanovic <polman@MacBook-Pro-van-polman.local>
2016-03-09 16:44:49 +0100

    bezig met versturen gegevens naar database

--- public/controllers/mainController.js
+++ public/controllers/mainController.js
@@ -1,13 +1,21 @@
 angular.module('mainController', [])
-    .controller('mainController', ['$scope', function ($scope) {
-
-      $scope.registreren = function()  {
-        
-        console.log('hello');
+    .controller('mainController', ['$scope', '$http', function ($scope, $http) {
 
 
+      // register functie, start wanneer register button geklikt is
+      $scope.register= function(){
+	      var data = $.param({
+	        book: JSON.stringify({
+	            author: $scope.username,
+	            title : $scope.password
+	        })
+	      });
 
-      };
+	      $http.post("/user/create/", data).success(function(data, status) {
+	        console.log('Data posted successfully');
+	      })
+	   }
+
 
 
 }]);
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
+
 connection.query('SELECT * from gebruikers', function(err, rows, fields) {
   if (!err)
     console.log('The solution is: ', rows);
@@ -21,9 +22,9 @@ connection.query('SELECT * from gebruikers', function(err, rows, fields) {
     console.log('Error while performing Query.');
 });
 
+// alle querys zijn uitgevoerd voordat de er een quit bericht 
+// gestuurd wordt naar mysql
 connection.end();
-
-
 
 app.listen(3000, function () {
   console.log('inlog functie on port 3000');
@@ -33,3 +34,17 @@ app.listen(3000, function () {
 });
 
 app.use(express.static('public'));
+
+
+// post data naar database
+app.post('/user/create', function(req, res, next){
+   var gebruikers = req.body.params;
+   var query = connection.query('insert into gebruikers set ?', gebruikers, function(err, result) {
+     if (err) {
+       console.error(err);
+       return res.send(err);
+     } else {
+       return res.send('Ok');
+     }
+});
+
