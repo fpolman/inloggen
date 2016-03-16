3b8a553bf7001c2dd86697aa2a2d4af053a985a2
polmanovic <polman@MacBook-Pro-van-polman.local>
2016-03-16 14:07:08 +0100

    layout website en vulling website

--- public/controllers/mainController.js
+++ public/controllers/mainController.js
@@ -1,35 +1,6 @@
 angular.module('mainController', [])
     .controller('mainController', ['$scope', '$http', function($scope, $http) {
 
-
-       // het laden van de landen
-	  /*$scope.load = function ()  {
-	    $http.get('/landen').
-	      success(function(data, status, headers, config) {
-	        $scope.landen = data;
-	      }).
-	      error(function(data, status, headers, config) {
-	      console.log(status);
-	      console.log(data);
-	      });
-	  };
-
-	  $scope.load();
-
-
-	  // toevoegen landen functie, start wanneer addLand button geklikt is
-      $scope.addLand = function(){
-
-	      $http.post("/add/land/", $scope.landen).success(function(status) {
-	        console.log('Data posted successfully');
-	        $scope.load();
-	      })
-
-    	console.log("addLand")
-
-	   }; */
-
-
       // register functie, start wanneer register button geklikt is
       $scope.register = function(){
 
